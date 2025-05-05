fcd1c5c5f137aa714d48b4a31c1a110399984f45
djballowe <djballowe@gmail.com>
2025-05-04 22:08:44 -0700

    update

--- rename_tv.sh
+++ rename_tv.sh
@@ -11,14 +11,14 @@ if [[ $# -ne 4 ]]; then
         exit 1
 fi
 
-folder_path="/mnt/mr-man/media/tv/"$folder"/season_"$season""
+folder_path="/mnt/media/television/"$folder"/season_"$season""
 if [[ ! -d "$folder_path" ]]; then
         read -p "The folder destinaion does not exist. Would you like to create it? (y/n): " response
         if [[ "$response" == "y" ]]; then
-                mkdir "$folder_path"
+                mkdir -p "$folder_path"
         else
                 echo "exiting without moving files"
-                        exit 1
+                exit 1
         fi
 fi
 
@@ -31,9 +31,13 @@ for file in "$DIRECTORY"/*; do
                 fi
 
                 new_name=""$title".S"$season".E"$episode_string".mkv"
+                full_path=""$folder_path"/"$new_name""
 
-                mv "$basename" "/"$folder_path"/"$title".S"$season"E"$episode""
-                echo "Renamed file "$basename" to "$new_name""
+                sudo mv "$base_name" "$full_path"
+                if [[ $? -ne 0 ]]; then
+                        echo "error occured moving file"
+                        exit 1
+                echo "Moved file: "$base_name" to "$full_path""
 
                 ((episode++))
         fi
